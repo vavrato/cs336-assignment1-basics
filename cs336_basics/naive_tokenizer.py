273fe5dec4ec78baa10e76315fc48aef41dbbbd9
tomas.vavra <tomas.vavra@homecredit.eu>
2025-07-31 19:30:49 +0200

    tokenizer refactorization, it passes almost all test

--- cs336_basics/naive_tokenizer.py
+++ cs336_basics/naive_tokenizer.py
@@ -1,25 +1,128 @@
-from typing import Any, Iterator,Optional
+import json
+from typing import Any, Iterable, Iterator, Optional, Self
 import regex as re
 from tqdm import tqdm
+from time import time
 #from pretokenization_example import find_chunk_boundaries
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
+def byte_level_tokenization(word: str) -> tuple[bytes,...]:
+    return tuple([bytes([byte]) for byte in word.encode()])
+
+class Tokenizer:
+    def __init__(
+            self, 
+            vocab: dict[int, bytes], 
+            merges: list[tuple[bytes, bytes]], 
+            special_tokens: Optional[list[str]] = None
+            ) -> None:
+        self.vocab = vocab
+        self.vocab_inverted = {v:k for k,v in vocab.items()}
+        self.merges = merges
+        self.merges_priority = {pair: i for i, pair in enumerate(merges)}
+        if not special_tokens:
+            special_tokens = []
+        self.special_tokens = special_tokens
+
+        for token in special_tokens:
+            if token not in self.vocab_inverted:
+                self.vocab_inverted[token.encode()] = len(vocab)
+
+
+    @classmethod
+    def from_files(
+        cls, 
+        vocab_filepath: str, 
+        merges_filepath: str, 
+        special_tokens: Optional[list[str]] = None
+    ) -> Self:
+        with open(merges_filepath, 'r') as f:
+            merges = [tuple(token.encode("utf-8") for token in line.rstrip().split(" ")) for line in f]
+            
+        with open(vocab_filepath, 'r') as f:
+            vocab = json.load(f)
+
+        if not special_tokens:
+            special_tokens = []
+
+        return cls(vocab, merges, special_tokens) # type: ignore
+    
+    @staticmethod
+    def _split_on_special_tokens(text: str, special_tokens: list[str]) -> list[str]:
+        special_tokens = [re.escape(token) for token in special_tokens]
+        pattern = f"({'|'.join(special_tokens)})"
+        # pattern = "|".join(special_tokens)
+        chunks = re.split(pattern, text)
+
+        return chunks
+    
+    def _pretokenize(self, text: str) -> Iterator[re.Match[str]]:
+        return re.finditer(PAT, text)
+
+    def encode(self, text: str) -> list[int]:
+        words_as_tuples = []
+        if self.special_tokens:
+            splits = self._split_on_special_tokens(text, self.special_tokens)
+        else:
+            splits = [text]
+        for sentence in splits:
+            if sentence not in self.special_tokens:
+                pretokens: Iterator = self._pretokenize(sentence)
+                for word_match in pretokens:
+                    word = word_match.group()
+                    word_as_byte_list = [bytes([byte]) for byte in word.encode()]
+                    word_as_byte_tuple = tuple(word_as_byte_list)
+                    words_as_tuples.append(word_as_byte_tuple)
+            else:
+                words_as_tuples.append((sentence.encode(),))
+        
+        token_sequence = []
+        for word in words_as_tuples:
+            word_tokenized = self.BPE_encode_one_word(word)
+            for token in word_tokenized:
+                token_sequence.append(self.vocab_inverted[token])
+
+        return token_sequence
+
+
+    def BPE_encode_one_word(self, word: tuple[bytes, ...]) -> tuple[bytes, ...]:
+        for pair in self.merges:
+            word = _merge_tokens(word, pair)
+
+        return word
+
+    def byte_encode_one_word(self, word: str) -> tuple[bytes, ...]:
+        return tuple([letter.encode() for letter in word])
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            result = self.encode(text)
+            for id in result:
+                yield id
+    
+    def decode(self, ids: list[int]) -> str:
+        bytestring = b''.join(self.vocab[id] for id in ids)
+        return bytestring.decode(errors='replace')
+
 class MyTokenizer:
     merges: list[tuple[bytes, bytes]]
-    vocabulary: dict[int, tuple[bytes,...]]
-    words_counts_dict = dict[tuple[bytes, ...], int]
-    pairs_counts_dict = dict[tuple[bytes, ...], int]
+    vocabulary: dict[int, bytes]
+    words_counts_dict: dict[tuple[bytes, ...], int]
+    pairs_counts_dict: dict[tuple[bytes, ...], int]
+    special_tokens: list[str]
 
-    def train(self, text, special_tokens, vocab_size: int) -> tuple[dict[int, tuple[bytes,...]], list[tuple[bytes, bytes]]]:
+    def train(self, text, special_tokens, vocab_size: int) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+        print('starting preprocessing')
+        start = time()
         words_counts_dict, pairs_counts_dict = self.make_counts_dicts(text, special_tokens)
-        
+        print('preprocessing took ', time() - start, ' seconds')
         vocab = [bytes([i]) for i in range(256)] + [token.encode() for token in special_tokens]
         merges = []
 
         n_steps = vocab_size - len(vocab)
 
-        for i in range(n_steps):
+        for i in tqdm(range(n_steps)):
             # do naive implementation first
             # merge the words, make it into new word counts and completely new pairs counts
             max_pair = self._get_maximal_token_pair(pairs_counts_dict)
@@ -28,13 +131,16 @@ class MyTokenizer:
 
             new_words_counts_dict: dict = {}
             for old_word, cnt in words_counts_dict.items():
-                new_word = self._merge_tokens(old_word, max_pair)
+                new_word = _merge_tokens(old_word, max_pair)
                 new_words_counts_dict[new_word] = cnt
 
             words_counts_dict = new_words_counts_dict
             pairs_counts_dict = self._make_pairs_count_dict(words_counts_dict)
 
-        vocab_dict = {i: token for i, token in enumerate(vocab)}
+        vocab_dict = {i: b''.join(tuple) for i, tuple in enumerate(vocab)}
+
+        self.vocabulary = vocab_dict
+        self.merges = merges
 
         return vocab_dict, merges
 
@@ -99,23 +205,33 @@ class MyTokenizer:
 
         return maximal_token_pair
     
-    @staticmethod
-    def _merge_tokens(word: tuple[bytes, ...], pair: tuple[bytes, bytes]) -> tuple[bytes,...]:
-        new_token = b''.join(pair)
-        new_word = []
-        l = len(word)
+def _merge_tokens(word: tuple[bytes, ...], pair: tuple[bytes, bytes]) -> tuple[bytes,...]:
+    new_token = b''.join(pair)
+    new_word = []
+    l = len(word)
 
-        i=0
-        while i < l-1:
-            if (word[i] == pair[0]) and (word[i+1] == pair[1]):
-                new_word.append(new_token)
-                i+=2
-            else:
-                new_word.append(word[i])
-                i+=1
-
-        if i == l-1:
+    i=0
+    while i < l-1:
+        if (word[i] == pair[0]) and (word[i+1] == pair[1]):
+            new_word.append(new_token)
+            i+=2
+        else:
             new_word.append(word[i])
+            i+=1
 
-        return tuple(new_word)
+    if i == l-1:
+        new_word.append(word[i])
 
+    return tuple(new_word)
+
+if __name__ == '__main__':
+    text = 'the cat ahojate'
+    vocab = {0: b' ', 1: b'a', 2:
+    b'c', 3: b'e', 4: b'h', 5: b't', 6: b'th', 7: b' c', 8: b' a', 9: b'the', 10: b' at'}
+    merges: list[tuple[bytes,bytes]] = [(b't', b'h'), (b' ', b'c'), (b' ', b'a'), (b'th', b'e'),(b' a', b't')]
+    special_tokens = ['ahoj']
+
+    tokenizer = Tokenizer(vocab, merges, special_tokens)
+    print(tokenizer.encode(text))
+    #print(tokenizer.vocab_inverted)
+    #print(tokenizer.decode([9, 7, 1, 5, 10, 3]))
